2152550e37d16e034099fe3a82a7cecaadbd5cb3
Edward Yu <3dward.yu@gmail.com>
2017-04-18 18:31:17 -0400

    add views for posts

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -3,20 +3,6 @@ var router = express.Router();
 
 var Blog = require('../../models/blog');
 
-var testBlog = new Blog({
-    title: 'Test',
-    author: 'Edward',
-    content: 'This is a test blog post',
-    hidden: false
-});
-
-testBlog.save(function(err){
-    if (err) throw err;
-
-    console.log('User created!');
-});
-
-
 // GET blog listing
 router.get('/posts', (req, res) => {
     Blog.find({}, function(err, blogs) {
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -4,7 +4,7 @@ import { PostsService } from './posts.service';
 @Component({
     selector: 'app-posts',
     templateUrl: './posts.component.html',
-    styleUrls: ['./posts.component.css']
+
 })
 
 export class PostsComponent implements OnInit { 
